fda871459de466c6d15d14b2d9c0aa0cefab5ab4
Jason Acquino <jasonacquino@Jasons-MacBook-Pro.local>
2014-12-11 09:33:35 -0500

    added boolean function and json data.

--- acquino_jason_project3.js
+++ acquino_jason_project3.js
@@ -22,14 +22,44 @@ var partyPlan = function (venuesSearched) {
     if (partyPlan === false) {
         console.log ("We have too many venues to pick from, the search continues!");
     } else {
-        console.log ("We have narrowed it to four venues, time to figure out which one will be best for our guests!");
+        console.log ("We have narrowed it to four venues.  Time to figure out which one will be best for our guests!");
     };
 };
+
+// Boolean Function
+var capacityData = function (json) {
+    for (var i = 0; i < json.capacities.length; i++){
+        var rightFit = json.capacities[i];
+        console.log("The " + rightFit.venue + " has a maximum capacity of " + rightFit.capacity + ".");
+    };
+};
+
+            var guestData = function (json2) {
+                for (var i = 0; i < json2.rsvps.length; i++){
+                    var guestsComing = json2.rsvps[i];
+                    console.log(guestsComing.name + " is definitely " + guestsComing.reply + ".");
+                };
+    
+            };
+                    var perfectVenue = function (guestAmount, capacity) {
+                        if (guestAmount <= 10 && capacity <= 10) {
+                        console.log("The " + venue[2] + " seems like the best place for this party.");
+                        } else
+                            console.log("The " + venue[3] + " seems like the best place for this party.");
+                            return perfectVenue = 10,10
+                    };
+                
+// Number Function
 
 
 
 
 // Main Code
 partyPlan(true);
+capacityData(json);
+guestData(json2);
+booleanReturn = perfectVenue(10,10);
+console.log(booleanReturn + " guests are coming!");
+
 
     
--- json.js
+++ json.js
@@ -1,1 +1,87 @@
-//json
+//Jason Acquino
+//SDI 1412 Project 3
+
+
+var json = {
+    "capacities": [
+        {
+            "venue": "yacht",
+            "capacity": 15
+        },
+        {
+            "venue": "house",
+            "capacity": 25
+        },
+        {
+            "venue": "apartment",
+            "capacity": 10
+        },
+        {
+            "venue": "tent",
+            "capacity": 50
+        }
+    ]  
+    
+};
+
+var json2 = {
+    "rsvps": [
+        {
+            "name": "Jerry",
+            "reply": "coming"
+        },
+        {
+            "name": "Larry",
+            "reply": "coming"
+        },
+        {
+            "name": "Dave",
+            "reply": "not coming"
+        },
+        {
+            "name": "Omar",
+            "reply": "coming"
+        },
+        {
+            "name": "Laura",
+            "reply": "coming"
+        },
+        {
+            "name": "Renne",
+            "reply": "coming"
+        },
+        {
+            "name": "Stacy",
+            "reply": "coming"
+        },
+        {
+            "name": "Moe",
+            "reply": "not coming"
+        },
+        {
+            "name": "Mark",
+            "reply": "not coming"
+        },
+        {
+            "name": "Jenn",
+            "reply": "coming"
+        },
+        {
+            "name": "Marlo",
+            "reply": "not coming"
+        },
+        {
+            "name": "Jimmy",
+            "reply": "coming"
+        },
+        {
+            "name": "Avon",
+            "reply": "coming"
+        },
+        {
+            "name": "Tanya",
+            "reply": "coming"
+        },
+        ]  
+    
+};
